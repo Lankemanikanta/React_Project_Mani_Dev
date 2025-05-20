76f61b17fc01fa0ae1a8356c2fa33d2cf6001c70
Lankemanikanta <lankemanikanta@gmail.com>
2025-05-20 15:53:44 +0530

    changed h1 tag

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Reset from './Reset';
+import Usestate from './usestate';
 function App (){
 
   return (
     <center>
       <Reset/>
+      <Usestate/>
     </center>
   )
 
---
+++ my-react-app/src/usestate.js
@@ -0,0 +1,14 @@
+
+import React ,{useState} from 'react'
+
+const Usestate = () => {
+    const [state ,setState] = useState("");
+    
+  return (
+    <div>usestate
+        <h1>vamsi is toppers</h1>
+    </div>
+  )
+}
+
+export default Usestate;
